b8e600a23d2b644f075e68200c89e21395f02b8c
Julian Gutierrez <julian.3.1@gmail.com>
2015-04-19 19:26:39 -0400

    Detects beacon

---
+++ HiBeacons/TARedeemCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TARedeemCodeViewController.h
+//  HiBeacons
+//
+//  Created by Julian Gutierrez Ferrara on 4/19/15.
+//  Copyright (c) 2015 Nick Toumpelis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TARedeemCodeViewController : UIViewController
+
+@end
